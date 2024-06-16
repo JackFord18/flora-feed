497a10f25d44349324d2bf291dabd9cf8de2571a
JackFord18 <jdford2@wisc.edu>
2024-06-16 00:25:58 -0500

    Added graph to display soil moisture data

---
+++ ui/MoistureGraph.js
@@ -0,0 +1,40 @@
+import React, { PureComponent } from 'react';
+import {
+    LineChart,
+    Line,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    Legend,
+    ResponsiveContainer,
+    ReferenceLine,
+} from "recharts";
+
+export default class MoistureGraph extends PureComponent {
+  render() {
+    return (
+      <ResponsiveContainer>
+        <LineChart data={this.props.data}>
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis scale='time' interval={'equidistantPreserveStart'} 
+        dataKey="timestamp" padding={{ left: 30, right: 30 }} 
+        tickFormatter={(value) => {
+            return new Date(value).toLocaleString('en-US');
+        }} 
+      />
+      <YAxis domain={[0,100]} />
+      <Tooltip formatter={(value) => [`${value}%`, "Moisture"]}
+      labelFormatter={(value) => {
+        return new Date(value).toLocaleString('en-US');
+      }}
+      />
+      <Legend />
+      <ReferenceLine y={this.props.maxSafeMoisture} stroke="red" label={{value: "Too wet", position: "top"}} />
+      <Line name={"Moisture"} type="monotone" dataKey="moisture" stroke="#5fc75b" dot={false} strokeWidth={5} />
+      <ReferenceLine y={this.props.minSafeMoisture} stroke="red" label={{value: "Too dry", position: "bottom"}} />
+    </LineChart>
+      </ResponsiveContainer>
+    );
+  }
+}

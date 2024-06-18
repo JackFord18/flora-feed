6a836fd01885c4a4e0729a9d2a98c13b0d7cfc95
JackFord18 <jdford2@wisc.edu>
2024-06-17 22:56:09 -0500

    Added home and dashboard tabs to the header component

--- ui/Header.js
+++ ui/Header.js
@@ -4,11 +4,21 @@ export default function Header({title}) {
   return (
     <div className='h-20 flex'>
         <div className='h-full w-2/3 flex items-center pl-20'>
-            <p className='text-2xl'>
+            <a href='/' className='text-2xl'>
                 Flora Feed
-            </p>
+            </a>
         </div>
         <div className='w-full justify-end flex space-x-20 pr-20'>
+        <div className='h-full flex items-center justify-center'>
+                <a href='/'>
+                    Home
+                </a>
+            </div>
+            <div className='h-full flex items-center justify-center'>
+                <a href='/dashboard' target='_blank'>
+                    Dashboard
+                </a>
+            </div>
             <div className='h-full flex items-center justify-center'>
                 <a href={"https://github.com/JackFord18/flora-feed"} target='_blank'>
                     Check out the GitHub!

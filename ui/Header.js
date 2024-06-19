c65a76218b66190aec3a826d2cb6886c250dc16c
JackFord18 <jdford2@wisc.edu>
2024-06-19 18:12:21 -0500

    Added logic for retrieving a random plant icon when initially rendering the header component on the client

--- ui/Header.js
+++ ui/Header.js
@@ -1,28 +1,46 @@
-import React from 'react'
+'use client';
+
+import React, { useEffect, useState } from 'react'
+import Image from 'next/image'
+import Link from 'next/link';
 
 export default function Header({title}) {
+    const [plantIconSource, setPlantIconSource] = useState();
+
+    useEffect(() => {
+        async function fetchPlantIcon() {
+            fetch('/api/randomPlantIcon')
+            .then(response => response.text())
+            .then(iconSource => {setPlantIconSource(iconSource)})
+            .catch(error => console.error('Encountered an error while fetching a random plant icon path:', error));
+        }
+        fetchPlantIcon();
+    }, []);
+
   return (
-    <div className='h-20 flex'>
-        <div className='h-full w-2/3 flex items-center pl-20'>
-            <a href='/' className='text-2xl'>
-                Flora Feed
-            </a>
-        </div>
+    <div className='h-20 flex pl-20 space-x-2'>
+            <Link href='/' className='flex h-full items-center w-2/3 text-2xl'>
+                    Flora
+                <div className='h-[65%] aspect-square relative mt-[-17px]'>
+                    {plantIconSource && <Image src={plantIconSource} alt='plant' fill/>}
+                </div>
+                    Feed
+        </Link>
         <div className='w-full justify-end flex space-x-20 pr-20'>
         <div className='h-full flex items-center justify-center'>
-                <a href='/'>
+                <Link href='/'>
                     Home
-                </a>
+                </Link>
             </div>
             <div className='h-full flex items-center justify-center'>
-                <a href='/dashboard' target='_blank'>
+                <Link href='/dashboard'>
                     Dashboard
-                </a>
+                </Link>
             </div>
             <div className='h-full flex items-center justify-center'>
-                <a href={"https://github.com/JackFord18/flora-feed"} target='_blank'>
+                <Link href={"https://github.com/JackFord18/flora-feed"} target='_blank'>
                     Check out the GitHub!
-                </a>
+                </Link>
             </div>
         </div>
         

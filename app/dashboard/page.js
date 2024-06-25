7c82c5cf05cdbc88128982fb79397f05a77944c0
JackFord18 <jdford2@wisc.edu>
2024-06-24 22:01:29 -0500

    Changed the dashboard page to be mobile-first and responsive

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -16,15 +16,15 @@ export default function DashboardPage() {
           .catch(error => console.error('Encountered an error while fetching soil moisture data:', error));
       }, []);
   return (
-    <div className='h-full flex flex-1 justify-center'>
-        <div className='w-2/3 h-2/3 flex flex-col bg-slate-100 p-10 rounded-xl'>
-            <p className='text-2xl text-center'>
+    <div className='flex my-2 md:my-5 justify-center'>
+        <div className='w-full md:w-4/5 h-72 flex flex-col px-1 rounded-xl'>
+            <p className='text-xl text-center'>
                 Soil Moisture
             </p>
             <p className='text-center'>
                 Soil moisture levels over the past 24 hours!
             </p>
-            <div className='flex-1'>
+            <div className='flex-1 touch-none'>
                 <MoistureGraph maxSafeMoisture={90} minSafeMoisture={35} data={data}/>
             </div>
         </div>

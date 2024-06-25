71df8acc58c7c790202072ebf40c7afc7307aee4
JackFord18 <jdford2@wisc.edu>
2024-06-24 22:00:39 -0500

    Changed the header to be mobile-first and responsive

--- ui/Header.js
+++ ui/Header.js
@@ -18,30 +18,26 @@ export default function Header({title}) {
     }, []);
 
   return (
-    <div className='h-20 flex flex-shrink-0 pl-20 space-x-2'>
-            <Link href='/' className='flex h-full items-center w-2/3 text-2xl'>
-                    Flora
-                <div className='h-[65%] aspect-square relative mt-[-17px]'>
+    <div className='border-solid border-b-2 sticky top-0 bg-white z-10 md:flex md:h-16 md:w-full md:pt-0 md:px-10'>
+        <div className='h-9 flex justify-center pt-1 md:h-full md:pt-1'>
+            <Link href='/' className='flex justify-center items-center text-2xl'>
+                Flora
+                <div className='h-2/3 md:h-1/2 aspect-square relative'>
                     {plantIconSource && <Image src={plantIconSource} alt='plant' fill/>}
                 </div>
-                    Feed
-        </Link>
-        <div className='w-full justify-end flex space-x-20 pr-20'>
-        <div className='h-full flex items-center justify-center'>
-                <Link href='/'>
-                    Home
-                </Link>
-            </div>
-            <div className='h-full flex items-center justify-center'>
-                <Link href='/dashboard'>
-                    Dashboard
-                </Link>
-            </div>
-            <div className='h-full flex items-center justify-center'>
-                <Link href={"https://github.com/JackFord18/flora-feed"} target='_blank'>
-                    Check out the GitHub!
-                </Link>
-            </div>
+                Feed
+            </Link>
+        </div>
+        <div className='flex h-10 md:flex-1 md:justify-end md:items-center md:h-full md:space-x-24'>
+            <Link href='/' className='flex flex-1 items-center justify-center text-center md:w-fit md:flex-none'>
+                Home
+            </Link>
+            <Link href='/dashboard' className='flex flex-1 items-center justify-center text-center md:w-fit md:flex-none'>
+                Dashboard
+            </Link>
+            <Link href={"https://github.com/JackFord18/flora-feed"} target='_blank' className='flex flex-1 items-center justify-center text-center md:w-fit md:flex-none'>
+                GitHub
+            </Link>
         </div>
         
     </div>
